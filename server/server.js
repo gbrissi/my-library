b8ab924e9e55f4751a51803bc180eda941e67d6f
Gabriel Rissi <gbrissi123@gmail.com>
2022-07-25 13:19:25 -0300

    update in api

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ app.use(express.json())
 
 async function main() {
 
-    /* INSERTING A "FICTIONAL ROW" IN users TABLE.
+    //INSERTING A "FICTIONAL ROW" IN users TABLE.
+    /* 
     await User.query().insert({
         username: 'KellyG',
         password: await bcrypt.hash('tomatoes', 10)
@@ -19,23 +20,6 @@ async function main() {
    const users = await User.query();
 
    //API
-
-   //IS IT GETTING THE QUERY AS A RESPONSE?
-   app.get('/users', (req, res) => {
-    res.json(users)
-   })
-
-   app.post('/users', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const user = { name: req.body.name, password: hashedPassword }
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
-   })
-
     app.post('/users/login', async (req, res) => {
         const person = users.find(person => person.username = req.body.username)
         if (person == null) {
@@ -59,7 +43,5 @@ main()
         console.error(err);
         return db.destroy();
         });
-
-
 
 app.listen(3001)
