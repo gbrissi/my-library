fffada6b24a8ccdc9e265e8fa5de076fefc22aff
Gabriel Rissi <gbrissi123@gmail.com>
2022-07-25 22:54:45 -0300

    update

--- server/util/_database.js
+++ server/util/_database.js
@@ -3,11 +3,11 @@ const { Model } = require('objection')
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
+        host: 'ec2-52-72-99-110.compute-1.amazonaws.com',
         port: 5432,
-        user: 'postgres',
-        password: 'admin',
-        database: 'my-library'
+        user: 'ajooirahwxgbhk',
+        password: 'cb090837e0a424357953e5a639ad0f5d2ad4e04c712239e7796d5fc07912b0f1',
+        database: 'd12e0eeoq116b4'
     }
   });
 
