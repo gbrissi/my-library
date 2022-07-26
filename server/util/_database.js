02b18e67ba3f8d47d6cbbe17931c821a8379e498
Gabriel Rissi <gbrissi123@gmail.com>
2022-07-26 12:41:43 -0300

    update

--- server/util/_database.js
+++ server/util/_database.js
@@ -2,13 +2,8 @@ const { Model } = require('objection')
 
 const knex = require('knex')({
     client: 'pg',
-    connection: {
-        host: 'ec2-52-72-99-110.compute-1.amazonaws.com',
-        port: 5432,
-        user: 'ajooirahwxgbhk',
-        password: 'cb090837e0a424357953e5a639ad0f5d2ad4e04c712239e7796d5fc07912b0f1',
-        database: 'd12e0eeoq116b4'
-    }
+    ssl: { require: false, rejectUnauthorized: false },
+    connection: process.env.DATABASE_URL
   });
 
 Model.knex(knex);
