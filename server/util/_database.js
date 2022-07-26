9a7916a1151f6bba8ce876a0f6c958f798c4703d
Gabriel Rissi <gbrissi123@gmail.com>
2022-07-26 12:42:53 -0300

    update

--- server/util/_database.js
+++ server/util/_database.js
@@ -2,7 +2,6 @@ const { Model } = require('objection')
 
 const knex = require('knex')({
     client: 'pg',
-    ssl: { require: false, rejectUnauthorized: false },
     connection: process.env.DATABASE_URL
   });
 
