ed21f488630555ea6eeb8a084dc53397d07819c3
Gabriel Rissi <gbrissi123@gmail.com>
2022-08-24 22:52:54 -0300

    trying to fix an error

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -3,7 +3,7 @@ const {knexSnakeCaseMappers} = require('objection');
 
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: process.env.DB_CLIENT || 'postgresql',
     connection: process.env.DATABASE_URL || {
       host: '127.0.0.1',
       database: 'my-library',
