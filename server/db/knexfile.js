bf1125b836aabbbb2c1ba95385e3a3bdf2a628a3
Gabriel Rissi <gbrissi123@gmail.com>
2022-08-25 12:10:33 -0300

    chore: fixing typo in environment variable

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,11 +1,10 @@
 // Update with your config settings.
 const {knexSnakeCaseMappers} = require('objection');
 
-let enviroment = process.env.DATABASE_URL ? {connectionString: process.env.DATABASE_URL, ssl:{rejectUnauthorized: false}} : {host: '127.0.0.1', database: 'my-library', user:'postgres', password:'admin'} 
-
+let environment = process.env.DATABASE_URL ? {connectionString: process.env.DATABASE_URL, ssl:{rejectUnauthorized: false}} : {host: '127.0.0.1', database: 'my-library', user:'postgres', password:'admin'} 
 module.exports = {
   client: 'pg',
-  connection: enviroment,
+  connection: environment,
   migrations: {
     directory: './migrations',
     tableName: 'knex_migrations'
