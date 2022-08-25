9fc212f64e8de63025b24391349ecad001609e3a
Gabriel Rissi <gbrissi123@gmail.com>
2022-08-25 12:09:39 -0300

    trying to fix API

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,17 +1,11 @@
 // Update with your config settings.
 const {knexSnakeCaseMappers} = require('objection');
 
+let enviroment = process.env.DATABASE_URL ? {connectionString: process.env.DATABASE_URL, ssl:{rejectUnauthorized: false}} : {host: '127.0.0.1', database: 'my-library', user:'postgres', password:'admin'} 
+
 module.exports = {
   client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-  } || {
-    host: '127.0.0.1',
-    database: 'my-library',
-    user: 'postgres',
-    password: 'admin'
-  },
+  connection: enviroment,
   migrations: {
     directory: './migrations',
     tableName: 'knex_migrations'
@@ -21,3 +15,10 @@ module.exports = {
   },
   ...knexSnakeCaseMappers
 };
+
+/*
+  {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  } ||
+*/
